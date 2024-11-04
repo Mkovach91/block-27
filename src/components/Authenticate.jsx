94da678d00a7331eebe6e06afc041be4ee42bcd1
matt k <matt.kovach91@gmail.com>
2024-11-04 07:37:38 -0600

    2nd commit

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,5 +1,30 @@
-const Authenticate = (props) => {
-  return <h2>Authenticate</h2>;
+const Authenticate = ({ token }) => {
+
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try { 
+      fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', { 
+        method: "GET", 
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": `${your-token-here}` 
+        }
+      })
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+            <h2>Authenticate</h2>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={handleClick}>Authenticate Token!</button>
+    </div>
+  );
 }
 
 export default Authenticate
